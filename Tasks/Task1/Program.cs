787aaa022f633591fc1b0c100391fdfdc8a841ef
AnyutaAntonovich <antonovich_am@bk.ru>
2023-05-24 08:30:24 +0600

    Add task1

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Input the three digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("The second digit of this number = "+stringNumber[1]);
