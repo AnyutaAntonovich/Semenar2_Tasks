4cad03446fc68f01a2dc738399f5655d65c120c2
AnyutaAntonovich <antonovich_am@bk.ru>
2023-05-25 07:59:57 +0600

    Add task 2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Input the number");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumberText = Convert.ToString(number);
+if (stringNumberText.Length > 2){
+  Console.WriteLine("third digit = " + stringNumberText[2]);
+}
+else {
+  Console.WriteLine("no third digit");
+}
